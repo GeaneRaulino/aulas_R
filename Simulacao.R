d4745539c777bdedb4dd02e0a72438748547a809
Geane <raulino.geane@gmail.com>
2022-08-21 16:23:45 -0300

    Testando alteracao de arquivo

--- Simulacao.R
+++ Simulacao.R
@@ -2,13 +2,13 @@ tarefaSemente <- addTaskCallback(function(...) {set.seed(123);TRUE})
 tarefaSemente
 
 #distribuicao normal simulada
-amostraNormal <- rnorm(100)
+amostraNormal <- rnorm(500)
 
 #sumario da distribuicao normal simulada
 summary(amostraNormal)
 
 # distribuicao binomial simulada
-amostraBinomial <- rbinom(100, 1, 0.7)
+amostraBinomial <- rbinom(800, 1, 0.7)
 
 #repeticoes
 classeSimulacao <- c(rep("Aluno", length(amostraBinomial)/3), rep("Monitor", length(amostraBinomial)/3), rep("Professor", length(amostraBinomial)/3))
