b58f8bbbba7d018eb1bad74d0d7e4c6f2a270a87
Geane <raulino.geane@gmail.com>
2022-08-27 08:32:33 -0300

    Index

--- Index_Operadores.R
+++ Index_Operadores.R
@@ -1,12 +1,20 @@
 #bibliotecas internas R
 data()
+pressure
+airquality
+#
+airquality[3,1] #Linha 3, coluna 1
+airquality[,'Wind'] #retorna todas a linhas da coluna Wind, que é a coluna 3
+airquality[, 'Ozone']
+
+CO2
+CO2[, 'Treatment']
+CO2[, 'Treatment', drop = FALSE]
 
 #operadores logicos
+airquality$Wind < 0
+which(airquality$Wind <= 10)
 
-USMortality
+match(airquality$Ozone, 22)
 
-USMortality$Rate < 0
-which(USMortality$Rate <= 10)
-which(USMortality$Rate == 13.9)
 
-match(USMortality$Cause, "Cancer")
