56dead140945203761026fac123ac773c73abc14
Geane <raulino.geane@gmail.com>
2022-08-27 11:13:06 -0300

    Objeto

---
+++ Objeto_Funcao.R
@@ -0,0 +1,10 @@
+#vetor nome do aluno
+nomeAluno <- c("Geane", "Maria", "João", "José")
+str(nomeAluno)
+
+#objeto complexo
+CO2
+CO2$uptake
+regCO2 <- lm(uptake ~ ., CO2) #objeto complexo
+str(regCO2) #complexidade do objeto
+
